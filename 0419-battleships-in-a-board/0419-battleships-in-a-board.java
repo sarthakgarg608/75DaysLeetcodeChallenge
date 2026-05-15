68ba737ee3156c398ba94ad62ce07aef8432d1d6
SarthakGarg <sarthakgarg608@gmail.com>
2026-05-15 10:00:31 +0530

    Time: 1 ms (97.89%), Space: 45.6 MB (40.2%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,12 +1,14 @@
 class Solution {
-    public void dfs(int r , int c , char[][] board , boolean[][] vis,int[] dr , int[] dc){
-        vis[r][c] = true;
+    public void dfs(int r , int c , char[][] board ,int[] dr , int[] dc){
+        //vis[r][c] = true;
+        board[r][c] = '.';
 
         for(int k = 0;k<4;k++){
             int nr = r+dr[k];
             int nc= c + dc[k];
-            if(nr >=0 && nr < board.length && nc >=0 && nc < board[0].length && board[nr][nc] == 'X' && !vis[nr][nc]){
-                dfs(nr,nc,board,vis,dr,dc);
+            if(nr >=0 && nr < board.length && nc >=0 && nc < board[0].length
+            && board[nr][nc] == 'X'){
+                dfs(nr,nc,board,dr,dc);
 
             }
         }
@@ -14,8 +16,8 @@ class Solution {
         
     }
     public int countBattleships(char[][] board) {
-        boolean[][] vis = new boolean[board.length][board[0].length];
-        for(boolean[] arr : vis) Arrays.fill(arr,false);
+        //boolean[][] vis = new boolean[board.length][board[0].length];
+        //for(boolean[] arr : vis) Arrays.fill(arr,false);
 
         int[] dr = {-1,0,1,0};
         int[] dc = {0,1,0,-1};
@@ -23,8 +25,8 @@ class Solution {
         int ans = 0;
         for(int i =0;i<board.length;i++){
             for(int j = 0;j<board[0].length;j++){
-                if(board[i][j] == 'X' && !vis[i][j]){
-                    dfs(i,j,board,vis,dr,dc);
+                if(board[i][j] == 'X'){
+                    dfs(i,j,board,dr,dc);
                     ans++;
                 }
             }
