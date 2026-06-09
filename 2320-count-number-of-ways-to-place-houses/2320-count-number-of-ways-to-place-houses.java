37af92bf744bd2ee843ffb33bc8125d1765f1157
SarthakGarg <sarthakgarg608@gmail.com>
2026-06-09 15:40:15 +0530

    Time: 10 ms (26.19%), Space: 46.4 MB (12.7%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -1,24 +1,19 @@
 class Solution {
     int n;
-    Integer[][] dp;
+    Integer[] dp;
     int mod;
-    public int solve(int p1 , int prevP1){
-        int ans = 0;
-        if(p1 == n) return 1;
-
-        if(dp[p1][prevP1] != null) return dp[p1][prevP1];
-        
-        if(prevP1 == 0) {
-            ans += (solve(p1+1,1)%mod+ solve(p1+1,0)%mod) % mod;
-        }else ans += solve(p1+1,0)%mod;
-
-        return dp[p1][prevP1] = ans%mod;
+    public int solve(int p1){
+        if(p1 >= n) return 1;
+        if(dp[p1] != null) return dp[p1];
+        int pick = solve(p1+2);
+        int notPick = solve(p1+1);
+        return dp[p1] = (pick+notPick)%mod;
     }
     public int countHousePlacements(int n) {
         this.n = n;
         mod = (int)(1e9+7);
-        dp = new Integer[n+1][2];
-        int way = solve(0,0)%mod;
+        dp = new Integer[n+1];
+        int way = solve(0)%mod;
         return (int)(((long)way * way) % mod);
 
         
