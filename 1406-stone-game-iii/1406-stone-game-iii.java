4d07e2f0807bb6195281b58ba2e93df35d8c94ce
SarthakGarg <sarthakgarg608@gmail.com>
2026-08-03 22:18:54 +0530

    Time: 35 ms (43.75%), Space: 93.7 MB (27.84%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,34 +1,24 @@
 class Solution {
     int[] nums;
     int n;
-    Integer[][] dp;
-    public int solve(int idx , int turn){ 
+    Integer[] dp;
+    public int solve(int idx){ 
         // if turn 1 that means Alice and 0 means bob
         if(idx == n ) return 0;
+        if(dp[idx] != null) return dp[idx];
+        int first = Integer.MIN_VALUE , second = Integer.MIN_VALUE , third = Integer.MIN_VALUE;
+
+        if(idx+1 <= n) first = (nums[idx] - solve(idx+1));
+        if(idx+2 <= n) second = (nums[idx]+ nums[idx+1] - solve(idx+2));
+        if(idx+3 <= n) third = (nums[idx]+nums[idx+1]+nums[idx+2] - solve(idx+3));
+        return dp[idx] =  Math.max(first,Math.max(second,third));
         
-
-        if(dp[idx][turn] != null) return dp[idx][turn];
-        if(turn == 1){
-            int first = Integer.MIN_VALUE , second = Integer.MIN_VALUE  , third = Integer.MIN_VALUE ;
-            if(idx+1 <= n) first = nums[idx] + solve(idx+1,0);
-            if(idx+2 <= n) second = nums[idx] + nums[idx+1] +  solve(idx+2,0);
-            if(idx+3 <= n) third  = nums[idx] + nums[idx+1] + nums[idx+2] +  solve(idx+3,0);
-
-            return dp[idx][turn] =  Math.max(first,Math.max(second,third));
-        }else {
-            int first = Integer.MAX_VALUE , second = Integer.MAX_VALUE  , third = Integer.MAX_VALUE ;
-            if(idx+1 <= n) first = -nums[idx] + solve(idx+1,1);
-            if(idx+2 <= n) second = -(nums[idx]  + nums[idx+1])  +  solve(idx+2,1);
-            if(idx+3 <= n) third  = -(nums[idx]  + nums[idx+1]  + nums[idx+2])  +  solve(idx+3,1);
-
-            return dp[idx][turn] =  Math.min(first,Math.min(second,third));
-        }
     }
     public String stoneGameIII(int[] nums) {
         this.nums = nums;
         this.n = nums.length;
-        this.dp = new Integer[n][2];
-        int score = solve(0,1);
+        this.dp = new Integer[n];
+        int score = solve(0);
         if(score > 0) return "Alice";
         else if(score < 0) return "Bob";
         else return "Tie";
