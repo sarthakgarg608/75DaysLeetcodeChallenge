76e35edef1bfcbdd4db70620e463940ef62920f2
SarthakGarg <sarthakgarg608@gmail.com>
2026-08-04 14:58:37 +0530

    Time: 54 ms (85.47%), Space: 99.4 MB (41.81%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {   
 
-        int n = dist.length;
-        int lo = 1;
-        int hi = (int)(1e7);
-        
+        int n = dist.length , lo = 1 , hi = (int)(1e7);
 
-        int min = Integer.MAX_VALUE; 
+        int minSpeed = Integer.MAX_VALUE; 
 
         while(lo <= hi){
             int mid = lo + (hi-lo)/2;
 
-            double totalHour = 0.0;
+            double totalHour = 0;
 
             for(int i =0 ; i < n ; i++){
-                if(i == n-1) totalHour += (dist[i]*1.0)/(mid*1.0);
+                if(i == n-1) totalHour += (dist[i]*1.0)/(mid);
                 else {
-                    if(dist[i] <= mid) totalHour += 1.0;
+                    if(dist[i] <= mid) totalHour += 1;
                     else {
                         if(dist[i] % mid == 0) totalHour += ((dist[i]*1.0) / mid);
                         else {
                             int div = dist[i] / mid;
-                            totalHour += (div*1.0+1.0);
+                            totalHour += ((div*1.0)+1);
                         }
                     }
                 }
@@ -29,13 +26,13 @@ class Solution {
             if(totalHour > hour) {
                 lo = mid+1;
             }else {
-                min = Math.min(min,mid);
+                minSpeed = Math.min(minSpeed,mid);
                 hi = mid-1;
 
             }
         }
-        if(min == Integer.MAX_VALUE) return -1;
-        return min;
+        if(minSpeed == Integer.MAX_VALUE) return -1;
+        return minSpeed;
 
 
     }
