510da2d21f5281a815058f99adbfa8789d9f0103
SarthakGarg <sarthakgarg608@gmail.com>
2026-08-05 23:35:26 +0530

    Time: 15 ms (56.43%), Space: 47.8 MB (17.84%) - LeetHub

---
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -0,0 +1,61 @@
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        
+        /**
+
+        In this problem statement we have to find that which player will win this game
+        As we Know in this problem for Alice "AAA" this substring must be present so that
+        he can remove character at middle 
+        for Bob "BBB" this substring must be in the original string 
+        we can find total group of string substrings "AAAA" like that and also for Bob 
+        we have to find substring like "BBBBB"  
+
+        */
+
+        
+        int ctAlice = 0;
+        int i = 0;
+        while(i < colors.length()){
+            if(colors.charAt(i) == 'A'){
+                int j = i+1;
+                while(j < colors.length()){
+                    if(colors.charAt(j) == 'A'){
+                        j++;
+                    }else break;
+                }
+                int len = j-i;
+                if(len > 2){
+                    ctAlice += (len-2);
+                }
+                i = j;
+
+
+            }else i++;
+        }
+
+        int ctBob = 0;
+        i = 0;
+        while(i < colors.length()){
+            if(colors.charAt(i) == 'B'){
+                int j = i+1;
+                while(j < colors.length()){
+                    if(colors.charAt(j) == 'B'){
+                        j++;
+                    }else break;
+                }
+                int len = j-i;
+                if(len > 2){
+                    ctBob += (len-2);
+                }
+                i = j;
+
+
+            }else i++;
+        }
+
+        if(ctAlice > ctBob) return true;
+        else return false;
+        
+
+    }
+}
