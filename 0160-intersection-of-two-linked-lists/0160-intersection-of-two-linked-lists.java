35702859a8fafe1ec18964da29981c794240822c
SarthakGarg <sarthakgarg608@gmail.com>
2026-09-02 13:21:27 +0530

    Time: 1 ms (99.91%), Space: 53.1 MB (12.4%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,19 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode , Integer> map = new HashMap<>();
-        ListNode temp = headA;
-        while(temp != null){
-            map.put(temp,1);
-            temp = temp.next;
+    public ListNode collisonPoint(ListNode temp1 , ListNode temp2, int d){
+        while(d != 0){
+            d--;
+            temp2 = temp2.next;
         }
 
-        temp = headB;
-        while(temp != null){
-            if(map.containsKey(temp)){
-                return temp;
-            }
-            temp = temp.next;
+        while(temp1 != temp2){
+            temp1 = temp1.next;
+            temp2 = temp2.next;
         }
-        return null;
+        return temp1;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        int n1 = 0 , n2 = 0;
+        while(temp1 != null){
+            n1++;
+            temp1 = temp1.next;
+        }
+
+        while(temp2 != null){
+            n2++;
+            temp2 = temp2.next;
+        }
+
+        // collisonPoint(smaller , greater , dist)
+
+        if(n1 < n2) return collisonPoint(headA,headB,n2-n1);
+        else return collisonPoint(headB,headA,n1-n2);
     }
 }
