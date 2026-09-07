1d3d5b9c8bc6a61be009fde7f1d8735d616c260c
SarthakGarg <sarthakgarg608@gmail.com>
2026-09-07 14:49:33 +0530

    Time: 1020 ms (40.5%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+WITH cte AS (
+    SELECT person_name,
+           turn,
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM cte
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
