b6f2a3386aa9d728defed34a3bd278293981c72f
SarthakGarg <sarthakgarg608@gmail.com>
2026-09-14 02:02:10 +0530

    Time: 304 ms (69.89%), Space: 0B (100%) - LeetHub

--- 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -1,11 +1,8 @@
 # Write your MySQL query statement below
-with cte as (select amount, transaction_date,
-row_number() over(partition by transaction_date  order by amount desc) as rnk
-from transactions)
 select transaction_date , 
 sum( case when amount%2 != 0 then amount else 0 end) as odd_sum,
 sum(case when amount%2 = 0 then amount else 0 end) as even_sum
-from cte
+from transactions
 group by transaction_date
 order by transaction_date;
 
