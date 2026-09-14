2a949229fd0e4be4fc7580938096ac6691667d85
SarthakGarg <sarthakgarg608@gmail.com>
2026-09-14 12:02:35 +0530

    Time: 758 ms (49.06%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,13 @@
+SELECT 
+    s.user_id,
+    ROUND(
+        IFNULL(
+            SUM(CASE WHEN c.action = 'confirmed' THEN 1 END),
+            0
+        ) / COUNT(*),
+        2
+    ) AS confirmation_rate
+FROM signups s
+LEFT JOIN confirmations c
+    ON s.user_id = c.user_id
+GROUP BY s.user_id;
